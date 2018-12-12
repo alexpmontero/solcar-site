36f5a5c54b735555c7d1c40241e6bf25585f00a3
Alex Perez Montero <alejandro.perez@dsindigo.com>
2018-12-12 14:25:35 -0600

    Cargar inicial

---
+++ js/main.js
@@ -0,0 +1,23 @@
+function slideHome() {
+	// var bgSlide1 = $(".extra img:nth-child(1)").attr("src");
+	// var	bgSlide2 = $(".extra img:nth-child(2)").attr("src");
+	// var	bgSlide3 = $(".extra img:nth-child(3)").attr("src");
+	// var	bgSlide4 = $(".extra img:nth-child(4)").attr("src");
+
+	$(".slide_home").vegas({
+		overlay: true,
+        delay: 5000,
+        slidesToKeep: 1,
+        transition: 'fade2',
+        transitionDuration: 8000,
+        animation: 'random',
+        animationDuration: 8000,
+        // overlay: "./imgs/07.png",
+	    slides: [
+	        { src: './imgs/bg_home_a.png'},
+	        { src: './imgs/bg_home_b.png'},
+	        { src: './imgs/bg_home_c.png'},
+	        { src: './imgs/bg_home_d.png'},
+	    ]
+	});
+}
